42d0eb57d40f1fca6eb3d229f33331a53bfde703
mndrs <mndrs.kc@gmail.com>
2022-01-13 14:47:08 -0500

    add style css

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,12 @@ var quizObject = [];
 var questionTextEl = document.querySelector("#question-text");
 var questionChoicesEl = document.querySelector("#question-choices");
 var questionAnswersEl = document.querySelector("#prev-question-answer");
+
+
+
+
+
+
 function initData() {
     //show only start page
     resetQuestionElements();
@@ -15,60 +21,116 @@ function initData() {
         "choices": [{
             "text": "setInterval(myTimer, 3)",
             "is_correct": false
-
         },
         {
             "text": "setInterval(myTimer, 30)",
             "is_correct": false
-
         },
         {
             "text": "setInterval(myTimer, 300)",
             "is_correct": false
-
         },
         {
             "text": "setInterval(myTimer, 3000)",
             "is_correct": true
-
         },
 
         ],
+
         "answer": "",
-
-
     },
         {
             "id": 1,
-            "question": "SECOND Q",
+            "question": "What value would we add to setInterval() if we want a function called, myTimer() to run every 3 seconds",
             "choices": [{
-                "text": "setInterval(myTimer, 3)",
+                "text": "event.preventDefault()",
                 "is_correct": false
-
             },
             {
-                "text": "setInterval(myTimer, 30)",
+                "text": "event.addEventListener()",
                 "is_correct": false
-
             },
             {
-                "text": "setInterval(myTimer, 300)",
+                "text": "event.target",
                 "is_correct": false
-
             },
             {
-                "text": "setInterval(myTimer, 3000)",
+                "text": "event.stopPropagation()",
                 "is_correct": true
-
             }],
+
             "answer": "",
+        },
+        {
+            "id": 2,
+            "question": "What operator is used to assign a value to a declared variable?",
+            "choices": [{
+                "text": "Equal sign (=)",
+                "is_correct": true
+            },
+            {
+                "text": "Colon (:)",
+                "is_correct": false
+            },
+            {
+                "text": "Double-equal (==)",
+                "is_correct": false
+            },
+            {
+                "text": "Question mark (?)",
+                "is_correct": true
+            }],
 
+            "answer": "",
+        },
+        {
+            "id": 3,
+            "question": "You just finished the feature that you've been working on a successfully merged your branch, feature-52. How would you delete branch, feature-52?",
+            "choices": [{
+                "text": "git branch -d feature-52",
+                "is_correct": true
+            },
+            {
+                "text": "git checkout feature-52",
+                "is_correct": false
+            },
+            {
+                "text": "git merge feature-52",
+                "is_correct": false
+            },
+            {
+                "text": "git branch feature-52",
+                "is_correct": true
+            }],
 
-        });
+            "answer": "",
+        },
+        {
+            "id": 4,
+            "question": "How do we declare a conditional statement in JavaScript?",
+            "choices": [{
+                "text": "	if...else",
+                "is_correct": true
+            },
+            {
+                "text": "for loop",
+                "is_correct": false
+            },
+            {
+                "text": "while loop",
+                "is_correct": false
+            },
+            {
+                "text": "difference...between",
+                "is_correct": true
+            }],
+
+            "answer": "",
+        }
+
+    );
 
     console.log("questiions are ready. Quiz reset !");
-
-
 }
 
 function startQuiz() {
@@ -99,7 +161,6 @@ function setQuestion(question_id) {
                 list.appendChild(textnode);
                 questionChoicesEl.appendChild(list);
 
-
             }
 
         }
@@ -126,8 +187,6 @@ function getAnswer(question_id, choice_id) {
     } else {
         quizObject[question_id].answer = "Wrong !";
     }
-
-
 
     setQuestion(next_question_id);
 
